9864c265b949ffea2b2b1014dac6e61545518446
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-06-14 11:45:17 -0500

    active prop replaced with activeLink

--- components/complex/NavBar.jsx
+++ components/complex/NavBar.jsx
@@ -10,36 +10,29 @@ const NavBar = ({ pathname }) => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <NavBarLink active={pathname} className='p-2' href='/'>
+                    <NavBarLink activeLink={pathname} className='p-2' href='/'>
                         Inicio
                     </NavBarLink>
-                    <NavBarLink active={pathname} className='p-2' href='/mascotas/perdidas'>
+                    <NavBarLink activeLink={pathname} className='p-2' href='/mascotas/perdidas'>
                         Mascotas perdidas
                     </NavBarLink>
-                    <NavBarLink active={pathname} className='p-2' href='/mascotas/adopcion'>
+                    <NavBarLink activeLink={pathname} className='p-2' href='/mascotas/adopcion'>
                         Mascotas en adopción
                     </NavBarLink>
-                    <NavBarLink active={pathname} className='p-2' href='blog'>
+                    <NavBarLink activeLink={pathname} className='p-2' href='blog'>
                         Blog
                     </NavBarLink>
-                    <NavBarLink active={pathname} className='p-2' href='signin'>
+                    <NavBarLink activeLink={pathname} className='p-2' href='signin'>
                         Registrarse
                     </NavBarLink>
-                    {/*<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>*/}
                 </Nav>
                 <Nav className="ml-auto">
-                    <NavBarLink active={pathname} className='p-2' href='login'>
+                    <NavBarLink activeLink={pathname} className='p-2' href='login'>
                         Iniciar sesion
                     </NavBarLink>
                 </Nav>
                 <Nav>
-                    <NavBarLink href='mascotas/publicar'>
+                    <NavBarLink href='mascotas/publicar' activeLink={pathname}>
                         <Button variant='outline-danger'>
                             Publicar
                         </Button>
