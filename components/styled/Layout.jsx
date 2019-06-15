a1b64992f9d86544ce7017b55e28b22844f3e0fe
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-06-14 19:23:28 -0500

    Buttons style added

--- components/styled/Layout.jsx
+++ components/styled/Layout.jsx
@@ -1,3 +1,5 @@
+import { primaryColor, primaryColorRGB } from "./Constants";
+
 const Layout = ({ children }) => {
     return (
         <div>
@@ -8,6 +10,38 @@ const Layout = ({ children }) => {
                 }
                 .family-montserrat {
                     font-family: 'Montserrat', sans-serif
+                }
+                .raised-button {
+                    color: #FFF;
+                    background: ${primaryColor};
+                    box-shadow: 0px 2px 4px rgba(${primaryColorRGB}, 0.25);
+                }
+                .raised-button:hover {
+                    color: #FFF;
+                    background: rgba(${primaryColorRGB}, 0.9);
+                    box-shadow: 0px 4px 4px rgba(${primaryColorRGB}, 0.25);
+                }
+                .raised-button:active {
+                    color: #FFF;
+                    background: ${primaryColor};
+                    box-shadow: 0px 2px 4px rgba(${primaryColorRGB}, 0.25);
+                }
+                .outlined-button {
+                    background: transparent;
+                    color: ${primaryColor};
+                    border: 2px solid ${primaryColor};
+                }
+                .outlined-button:hover {
+                    background: rgba(${primaryColorRGB}, 0.1);
+                    border: 2px solid rgba(${primaryColorRGB}, 0.8);
+                    color: ${primaryColor};
+                    box-shadow: none;
+                }
+                .outlined-button:active {
+                    background: rgba(${primaryColorRGB}, 0.3) !important;
+                    border: 2px solid ${primaryColor} !important;
+                    color: ${primaryColor} !important;
+                    box-shadow: none;
                 }
             `}</style>
         </div>
