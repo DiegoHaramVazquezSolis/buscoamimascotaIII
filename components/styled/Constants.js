1b350db91f9eaeaf8909a1006f45ebb0db392802
DiegoHaramVazquezSolis <diegovazquezo@live.com.mx>
2019-06-15 09:25:37 -0500

    Constants file added

---
+++ components/styled/Constants.js
@@ -0,0 +1,4 @@
+export const primaryColor = '#FDB963';
+export const primaryColorRGB = '253, 185, 99';
+export const primaryVariantColor = '#C78935';
+export const primaryVariantColorRGB = '199, 137, 53';
