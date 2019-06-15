c017f9d9cffe8df3232b51d3ecb3a952a2a0b002
DiegoHaramVazquezSolis <diegovazquezo@live.com.mx>
2019-06-15 11:17:28 -0500

    user flag on _app.js to pass it as prop to any page

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import App, { Container } from 'next/app'
 import Layout from '../components/styled/Layout';
+import { auth } from '../firebase/firebase';
 
 class MyApp extends App {
+  state = {
+    user: null
+  };
+  componentDidMount(){
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({user: true});
+      } else {
+        this.setState({user: false});
+      }
+    });
+  }
+
   render() {
     const { Component, pageProps } = this.props
 
     return (
       <Container>
         <Layout>
-          <Component {...pageProps} />
+          <Component {...pageProps} user={this.state.user} />
         </Layout>
       </Container>
     )
