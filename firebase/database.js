48179b081c034baeca4ca697376cadd5989d84df
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-06-22 17:22:00 -0500

    Functions to load mascotas perdidas added

--- firebase/database.js
+++ firebase/database.js
@@ -35,6 +35,11 @@ export async function publishMascotaPerdida(mascotaInfo){
     return await database.child('Perdidas').push(mascotaInfo);
 }
 
+/**
+ * Update information of a mascota perdida on database
+ * @param {string} key key of the child to update on Perdidas firebase node
+ * @param {object} infoToUpdate JSON object with info to update on the firebase selected node
+ */
 export async function updateMascotaPerdida(key, infoToUpdate) {
     return await database.child('Perdidas').child(key).update(infoToUpdate);
 }
@@ -45,4 +50,23 @@ export async function updateMascotaPerdida(key, infoToUpdate) {
  */
 export async function savePlaceOfPerdidaOnDatabase(place) {
     database.child('PerdidasPlaces').update({ [place]: place });
+}
+
+/**
+ * Get the list of places where are Perdidas reported
+ */
+export async function getPerdidasPlaces() {
+    return await database.child('PerdidasPlaces').once('value');
+}
+
+/**
+ * Return an object of mascotasPerdidas based on their desaparition place
+ * @param {string} place String for index the mascotasPerdidas
+ */
+export async function loadMascotasPerdidasOnce(place) {
+    if (place !== '') {
+        return await database.child('Perdidas').orderByChild('place').equalTo(place).once('value');
+    } else {
+        return Promise.reject({ errorLaunchedForVoidQuery: true });
+    }
 }
