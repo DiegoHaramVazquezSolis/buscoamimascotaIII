5edeefa63d4f63d871fe0e1ffe2a9eaeda821d54
DiegoHaramVazquezSolis <diegovazquezo@live.com.mx>
2019-06-23 17:50:27 -0500

    PerdidasLoad and search by place tests created and passed

---
+++ cypress/integration/Mascotas/Perdidas/PerdidasChangePlaceInput.spec.js
@@ -0,0 +1,16 @@
+describe('Publicar complete', function() {
+    beforeEach(() => {
+        cy.fixture('mascotasPerdidas').as('publicar');
+    });
+    it('Publish a mascota perdida', function() {
+        cy.visit('/mascotas/perdidas');
+        cy.wait(1000);
+
+        cy.get('#Query')
+            .clear()
+            .type('Zapopan, Jalisco, Mexico')
+            .should('have.value', 'Zapopan, Jalisco, Mexico');
+        cy.contains('Kenji de Zapopan');
+        cy.contains('Es igual a Kenji pero este es de Zapopan');
+    });
+});
---
+++ cypress/integration/Mascotas/Perdidas/PerdidasLoad.spec.js
@@ -0,0 +1,14 @@
+describe('Publicar complete', function() {
+    beforeEach(() => {
+        cy.fixture('mascotasPerdidas').as('publicar');
+    });
+    it('Publish a mascota perdida', function() {
+        cy.visit('/mascotas/perdidas');
+        cy.wait(500);
+
+        cy.contains('Garfield');
+        cy.contains('Garfield es un gato gordo y naranja, le encanta la lasagna pero odia los lunes. Estamos muy preocupados y necesitamos encontrarlo, mi perro Odie lo extraña muchísimo.');
+        cy.get('#Query')
+            .should('have.value', 'Guadalajara, Jalisco, Mexico');
+    });
+});
