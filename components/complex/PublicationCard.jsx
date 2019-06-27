1d5a99c6d06ef19f5bf89aa01eb9b10623a0826a
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2019-06-26 19:26:22 -0500

    Manual merge and Load mascota perdida test passed

--- components/complex/PublicationCard.jsx
+++ components/complex/PublicationCard.jsx
@@ -7,9 +7,9 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Button from 'react-bootstrap/Button';
 import TextForButtons from '../styled/TextForButtons';
 
-const PublicationCard = ({ image, date, name, description }) => {
+const PublicationCard = ({ image, date, name, description, onVerMasClick, onContactarClick }) => {
     return (
-        <Card className='mb-4'>
+        <Card className='mt-4'>
             <CardImage src={image} />
             <Body2 className='pt-2 mb-0 pl-2 text-muted'>{date}</Body2>
             <CardHeader>{name}</CardHeader>
@@ -18,14 +18,14 @@ const PublicationCard = ({ image, date, name, description }) => {
                     {description.length > 183 ? `${description.substring(0, 184)}...` : description}
                 </CardBody>
             </div>
-            <ButtonToolbar className='mt-2'>
-                <Button className='text-button mb-3 ml-2' size='sm' variant='none'>
+            <ButtonToolbar className='mt-3 ml-auto'>
+                <Button className='text-button mb-3 ml-2' size='sm' variant='none' onClick={onVerMasClick}>
                     <TextForButtons>Ver mas</TextForButtons>
                 </Button>
-                <Button className='raised-button mb-3 ml-3' size='sm' variant='none'>
+                <Button className='raised-button mb-3 ml-2' size='sm' variant='none' onClick={onContactarClick}>
                     <TextForButtons>Contactar</TextForButtons>
                 </Button>
-                <Button className='text-button mb-3 ml-3' size='sm' variant='none'>
+                <Button className='text-button mb-3 ml-2 mr-2' size='sm' variant='none'>
                     <i className='fas fa-share-alt fa-2x'></i>
                 </Button>
             </ButtonToolbar>
